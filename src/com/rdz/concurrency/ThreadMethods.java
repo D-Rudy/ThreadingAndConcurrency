df19c63737c0ce2700b6cade338e1163eaa6eac5
D-Rudy <duriez.ry@gmail.com>
2024-05-27 10:25:05 +0200

    Thread priority

--- src/com/rdz/concurrency/ThreadMethods.java
+++ src/com/rdz/concurrency/ThreadMethods.java
@@ -2,20 +2,6 @@ package com.rdz.concurrency;
 
 public class ThreadMethods {
 
-	public static class Walk implements Runnable {
-		@Override
-		public void run() {
-			for (int i = 0; i < 5; i++) {
-				try {
-					Thread.sleep(3000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-				System.out.println("Je marche");
-			}
-		}
-	}
-
 	public static class ChewGum implements Runnable {
 		@Override
 		public void run() {
@@ -30,32 +16,52 @@ public class ThreadMethods {
 		}
 	}
 
+	public static class Walk implements Runnable {
+		@Override
+		public void run() {
+			for (int i = 0; i < 5; i++) {
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("Je marche");
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		
+
 		Thread walkThread = new Thread(new Walk());
 		Thread chewGum = new Thread(new ChewGum());
+
+		System.out.println("\nPriorité de walkThread: " + walkThread.getPriority());
+		System.out.println("Priorité thread de chewGum: " + chewGum.getPriority());
+		
+		walkThread.setPriority(9);
+		chewGum.setPriority(2);
+
+//		System.out.println("\nId de walkThread: " + walkThread.getId());
+//		System.out.println("Id de chewGum: " + chewGum.getId());
+//		System.out.println("Id de main: " + Thread.currentThread().getId());
+//
+//		System.out.println("\nNom de walkThread: " + walkThread.getName());
+//		System.out.println("Nom de chewGum: " + chewGum.getName());
+//		System.out.println("Nom de main: " + Thread.currentThread().getName());
+//
+//		System.out.println("\nGroupe thread de walkThread: " + walkThread.getThreadGroup());
+//		System.out.println("Groupe thread de chewGum: " + chewGum.getThreadGroup());
+//		System.out.println("Groupe thread de main: " + Thread.currentThread().getThreadGroup());
+
+		System.out.println("\nPriorité de walkThread: " + walkThread.getPriority());
+		System.out.println("Priorité thread de chewGum: " + chewGum.getPriority());
+		System.out.println("Priorité thread de main: " + Thread.currentThread().getPriority());
 
 		walkThread.start();
 		chewGum.start();
 		
-		System.out.println("\nId de walkThread: " + walkThread.getId());
-		System.out.println("Id de chewGum: " + chewGum.getId());
-		System.out.println("Id de main: " + Thread.currentThread().getId());
+//		System.out.println("\nThread Actifs: " + Thread.activeCount());
 
-		System.out.println("\nNom de walkThread: " + walkThread.getName());
-		System.out.println("Nom de chewGum: " + chewGum.getName());
-		System.out.println("Nom de main: " + Thread.currentThread().getName());
-		
-		System.out.println("\nGroupe thread de walkThread: " + walkThread.getThreadGroup());
-		System.out.println("Groupe thread de chewGum: " + chewGum.getThreadGroup());
-		System.out.println("Groupe thread de main: " + Thread.currentThread().getThreadGroup());
-		
-		System.out.println("\nPriorité de walkThread: " + walkThread.getPriority());
-		System.out.println("Priorité thread de chewGum: " + chewGum.getPriority());
-		System.out.println("Priorité thread de main: " + Thread.currentThread().getPriority());
-		
-		System.out.println("\nThread Actifs: " + Thread.activeCount());
-		
 		System.out.println("\n\n");
 	}
 
